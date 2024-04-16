8dd072554963bacb53c780096babe09a01f99fb3
douggy81 <42162036+douggy81@users.noreply.github.com>
2024-04-16 00:06:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,8 +71,6 @@ index=get_index()
 if "chat_engine" not in  st.session_state.keys():
     postprocessor = SentenceEmbeddingOptimizer(embed_model=Settings.embed_model,percentile_cutoff=0.5, threshold_cutoff= 0.7)
 
-
-
     st.session_state.chat_engine = index.as_chat_engine(
         chat_mode="context",
         verbose = True,
@@ -93,7 +91,7 @@ st.title("Chat with the Gemini, your personal trainer in sales using a methodolo
 st.divider()
 
 #Language Selection:
-language_select = st.selectbox(label="Language", options=["English", "Francais"], index = 0)
+language_select = st.selectbox(label="Language", options=["English", "Français"], index = 0)
 
 if prompt:= st.chat_input("Your question"):
     st.session_state.messages.append({
@@ -104,8 +102,8 @@ if "messages" not in st.session_state.keys():
     st.session_state.messages=[
         {
             "role": "assistant",
-            "content" : "Ask me a question about the Art of Sale book."
-                        "Posez moi vos questions sur le livre Manuel de Formation a la vente"
+            "content" : if language_select=="English" then: "Ask me a question about the Art of Sale book."
+                        else if language_select=="Français" then: Posez moi vos questions sur le livre Manuel de Formation a la vente"
         }
     ]
 for message in st.session_state.messages:
