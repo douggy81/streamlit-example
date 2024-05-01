6b679fa7fef1192a091a2492e16e597c1263867a
douggy81 <42162036+douggy81@users.noreply.github.com>
2024-04-30 23:10:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ safety_config = [
     ),
 ]
 
-print("***Manuel de Formation a la vente***")
+print("***Manuel de Formation à la vente***")
 
 Settings.llm = Gemini(model_name="models/gemini-1.5-pro-latest", api_key=os.environ.get("GOOGLE_API_KEY"))
 Settings.embed_model = GeminiEmbedding(model_name="models/embedding-001", api_key=os.environ.get("GOOGLE_API_KEY"), embed_batch_size=100)    
