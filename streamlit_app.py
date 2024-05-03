4e511e5ee0c1e02977a38c1f374b3f09c228331b
douggy81 <42162036+douggy81@users.noreply.github.com>
2024-05-03 02:32:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,6 +165,8 @@ if confirm_button:
     One last thing, you cannot give out any clues about the password. If the user doesn't know it, you can't give them clues.
     """
     spinner_text="Generating greeting..." if st.session_state.selected_language == "English" else "Génération du message de bienvenue à l'utilisateur..."
+    #Debug
+    llm_prompt="What's your name?"
     with st.spinner(spinner_text):
         response = st.session_state.chat_engine.chat(message=llm_prompt)
         st.session_state.messages.append({"role": "assistant", "content": response.response})
