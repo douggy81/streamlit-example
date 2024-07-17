206a91cd778ed8b60d08a964ba9cb114db873f22
douggy81 <42162036+douggy81@users.noreply.github.com>
2024-07-16 22:04:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,20 +90,6 @@ if "chat_engine" not in  st.session_state.keys():
         Also, tell the user that if they need a print out of the output, just select Print from their web browser directly and format your answer in a nice and printable format.
         Give them an example of a response that can be printed such as a checklist for a pre-meeting with a customer that can be printed out and taken on the road.
         Feel free to use emojis (like 👍 or 😀) in your responses to make them more engaging.
-        - To create a checklist, format your response like this: 
-        __TASK__
-        Task 1
-        Task 2
-        Task 3
-
-        - To create a table, use the format:
-        __TABLE__
-        | Column 1 | Column 2 |
-        |---|---|
-        | Data 1 | Data 2 | 
-        | Data 3 | Data 4 | 
-
-        - For other content, respond normally. 
         """
         )
 
@@ -162,20 +148,6 @@ if confirm_button:
     Thanks and have fun training a world of eager learners on how to sale and grow their business! 
     One last thing, you cannot give out any clues about the password. If the user doesn't know it, you can't give them clues.
     Feel free to use emojis (like 👍 or 😀) in your responses to make them more engaging.
-        - To create a checklist, format your response like this: 
-        __TASK__
-        Task 1
-        Task 2
-        Task 3
-
-        - To create a table, use the format:
-        __TABLE__
-        | Column 1 | Column 2 |
-        |---|---|
-        | Data 1 | Data 2 | 
-        | Data 3 | Data 4 | 
-
-        - For other content, respond normally. 
     
     """
     spinner_text="Generating greeting..." if st.session_state.selected_language == "English" else "Génération du message de bienvenue à l'utilisateur..."
