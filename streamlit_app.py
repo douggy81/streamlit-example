067b2c1b55b5475a7c87e838c2558f6645a146a5
douggy81 <42162036+douggy81@users.noreply.github.com>
2024-07-16 22:38:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,8 @@ def update_title():
     st.title(title_text)
 
 if "chat_engine" not in  st.session_state.keys():
-    postprocessor = SentenceEmbeddingOptimizer(embed_model=Settings.embed_model,percentile_cutoff=0.5, threshold_cutoff= 0.7)
+    #postprocessor = SentenceEmbeddingOptimizer(embed_model=Settings.embed_model,percentile_cutoff=0.5, threshold_cutoff= 0.7)
+    postprocessor = SentenceEmbeddingOptimizer(embed_model=Settings.embed_model,percentile_cutoff=1, threshold_cutoff= 1)
 
     st.session_state.chat_engine = index.as_chat_engine(
         chat_mode="context",
