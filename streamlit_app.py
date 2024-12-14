89f53fff2b6701834cac68eb9a592dc03e5c2fba
douggy81 <42162036+douggy81@users.noreply.github.com>
2024-12-14 12:35:44 -0500

    updated create word document

--- streamlit_app.py
+++ streamlit_app.py
@@ -209,9 +209,10 @@ def create_word_document(formatted_text):
         soup = BeautifulSoup(html, 'html.parser')
 
         if soup.body: # check if soup.body exists
-            if soup.body.contents: #Check if there are contents
-              for element in soup.body.contents:
-                    if element.name == 'p':
+            try: # attempt to access soup.body.contents inside a try/except
+              if soup.body.contents: #Check if there are contents
+                for element in soup.body.contents:
+                      if element.name == 'p':
                         for item in element.contents:
                             if str(item).startswith('<strong>'):
                                 p.add_run(item.text).bold = True
@@ -219,7 +220,10 @@ def create_word_document(formatted_text):
                                 p.add_run(item.text).italic = True
                             else:
                                 p.add_run(str(item))
-            else: # if soup.body does not contain anything, add the line
+              else: # if soup.body does not contain anything, add the line
+                document.add_paragraph(line)
+
+            except AttributeError: # if soup.body does not have attribute 'contents', just add the line
                document.add_paragraph(line)
         else: # if soup.body does not exist, add the line as normal paragraph
             document.add_paragraph(line)
